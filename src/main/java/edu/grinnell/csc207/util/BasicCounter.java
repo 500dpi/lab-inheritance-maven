2e65b621ebadf1eb5376c28fedfc029f5cb606aa
Sara_Jaljaa <jaljaasa@grinnell.edu>
2024-10-01 09:05:39 -0500

    Add BasicCounter, along with some tests and some experiments

---
+++ src/main/java/edu/grinnell/csc207/util/BasicCounter.java
@@ -0,0 +1,31 @@
+package edu.grinnell.csc207.util;
+
+public class BasicCounter implements Counter{
+  protected int count;
+  protected int start;
+
+  public BasicCounter(int val) {
+    this.count = val;
+    this.start = val;
+  }
+
+  public void increment() throws Exception{
+    if (this.count > Integer.MAX_VALUE) {
+      throw new Exception ("Value too large");
+    } //if
+    this.count = this.count + 1;
+  } //increment()
+
+  public void reset() {
+    this.count = this.start;
+  } //reset()
+
+  public String toString() {
+    return "[" + this.count + "]";
+  }
+
+  public int get() {
+    return this.count;
+  }
+
+}
